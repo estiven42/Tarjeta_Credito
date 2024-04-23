3de002f6df3df7d24e5cccca1d1ca7f26a44ea32
estiven42 <notavop@ucentral.edu.co>
2024-04-23 18:28:37 -0500

    Primer commit se sube proyec

---
+++ src/main/java/com/ucentral/Tarjeta_Credito/TarjetaCreditoApplication.java
@@ -0,0 +1,13 @@
+package com.ucentral.Tarjeta_Credito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarjetaCreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarjetaCreditoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucentral/Tarjeta_Credito/TarjetaCreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucentral.Tarjeta_Credito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarjetaCreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
